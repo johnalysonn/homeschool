4febde013c750647beefa18dd07ee0d5c2972f0f
John Alyson <johnalyson6633@gmail.com>
2023-01-02 16:04:15 -0300

    notes action in progress

--- resources/views/activity/showActivity.blade.php
+++ resources/views/activity/showActivity.blade.php
@@ -25,21 +25,35 @@
                             </p>
                             <p>
                                 @if ($response->check===0)
-                                    <a href="/home/response/status/{{$response->id}}/{{1}}" style="text-decoration: none; color: white;">Check</a>
+                                    <a href="/home/response/status/{{$response->id}}/{{1}}" id="check-link" style="text-decoration: none; color: white;">Check</a>
                                 @else
-                                    <a href="/home/response/status/{{$response->id}}/{{0}}"><i class="fa-solid fa-check" style="color: green;"></i></a>
+                                    <a href="/home/response/status/{{$response->id}}/{{0}}" id="check-link" ><i class="fa-solid fa-check" style="color: green;"></i></a>
                                 @endif
 
                             </p>
                         </div>
                         <div>
-                            <p><b>Aluno: </b> {{$response->student()->get()->first()->name}}</p>
+                            <p class="d-flex" style="justify-content: space-between;  text-align:center;">
+                                <label><b>Aluno: </b> {{$response->student()->get()->first()->name}}</label>
+                                <select name="note" id="input-note" style="transform: translateY(-10%)" onchange="noteChange()">
+                                    <option value="note" selected disabled>Nota</option>
+                                    <option value="0">0</option><option value="1">1</option>
+                                    <option value="2">2</option><option value="3">3</option>
+                                    <option value="4">4</option><option value="5">5</option>
+                                    <option value="6">6</option><option value="7">7</option>
+                                    <option value="8">8</option><option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </p>
                             <p><b>Observação: </b> {{$response->description}}</p>
                             <div class="div-download">
                                 <p><b>Arquivos: </b>  {{$response->filepath}}</p>
                                 <p class="p-down">
                                     <a href="/home/response/download/{{$response->id}}"><i class="fa-solid fa-arrow-down" style="color: red;"></i></a>
                                 </p>
+                            </div>
+                            <div id="container-button-note">
+                                <input type="submit" class="d-none" value="Salvar">
                             </div>
                         </div>
                     </div>
@@ -105,12 +119,25 @@
     var arrow = document.querySelector("#arrow");
     var container = document.querySelector("#sidebar-response");
 
+
     arrow.onclick = e =>{
         if(!container.classList.contains('ocult')){
             container.classList.add('ocult');
         }else{
             container.classList.remove('ocult');
         }
+    }
+    function noteChange(){
+        var note = document.querySelector('#input-note').value;
+        var container_note = document.querySelector('#container-button-note');
+
+        if(note !== '0'){
+        if(!container_note.classList.contains('d-none')){
+            container_note.classList.add('d-none');
+        }else{
+            container_note.classList.remove('d-none');
+        }
+    }
     }
 </script>
 @endsection
